949edf65b39324f0634f410e1cf0e92da3aafc8f
gaponukz <gaponukz54@gmail.com>
2023-07-22 18:37:50 +0300

    Add AddRoutesHandler

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from fastapi import FastAPI
 from src.db.json_db import RouteRepository
 from src.usecases.view_routes import ViewRoutesUseCase
 from src.usecases.route_availability import RouteAvailabilityUseCase
+from src.usecases.add_routes import AddRoutesUseCase
 
+from src.handlers.add_routes import AddRoutesHandler
 from src.handlers.route_availability import RouteAvailabilityHandler
 from src.handlers.view_routes import ViewRoutesHandler
 
 db = RouteRepository("routes.json")
 view_usecase = ViewRoutesUseCase(db)
 availability_usecase = RouteAvailabilityUseCase(db)
+add_routes_usecase = AddRoutesUseCase(db)
 
+add_routes_handler = AddRoutesHandler(add_routes_usecase)
 availability_handler = RouteAvailabilityHandler(availability_usecase)
 view_handler = ViewRoutesHandler(view_usecase)
 
 app = FastAPI()
 app.include_router(view_handler.router)
 app.include_router(availability_handler.router)
+app.include_router(add_routes_handler.router)
