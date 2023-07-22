8697338bc5e84f51b1bd4aed90be4104f7f31838
gaponukz <gaponukz54@gmail.com>
2023-07-22 17:16:46 +0300

    Add EditRoutersUseCase and tests

--- src/usecases/edit_routes.py
+++ src/usecases/edit_routes.py
@@ -1,3 +1,13 @@
+import typing
+from src.business.entities import Route
+from src.business.dto import UpdateRoutesDTO
+
+class UpdateAbleDataBase(typing.Protocol):
+    def update(self, route: Route): ...
 
 class EditRoutersUseCase:
-    pass
+    def __init__(self, db: UpdateAbleDataBase):
+        self._db = db
+    
+    def update(self, dto: UpdateRoutesDTO):
+        self._db.update(dto.route)
