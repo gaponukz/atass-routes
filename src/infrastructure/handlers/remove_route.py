b42ba1186858758bb954813ed5bfadd37d606c36
gaponukz <gaponukz54@gmail.com>
2023-08-18 00:10:41 +0300

    Devide update/delete

--- src/infrastructure/handlers/remove_route.py
+++ src/infrastructure/handlers/remove_route.py
@@ -6,25 +6,13 @@ from src.application.dto import UpdateRoutesDTO
 class DeleteRouteService(typing.Protocol):
     def delete(self, route_id: HashId): ...
 
-class EditRoutersService(typing.Protocol):
-    def update(self, dto: UpdateRoutesDTO): ...
 
-
-class ChangeRoutesHandler:
-    def __init__(
-            self,
-            update_service: EditRoutersService,
-            delete_service: DeleteRouteService
-    ):
-        self._update_service = update_service
+class RemoveRouteHandler:
+    def __init__(self, delete_service: DeleteRouteService):
         self._delete_service = delete_service
         self.router = APIRouter()
 
-        self.router.add_api_route("/routes", self.update, methods=["UPDATE"])
         self.router.add_api_route("/routes", self.detete, methods=["DELETE"])
-
-    def update(self, dto: UpdateRoutesDTO):
-        self._update_service.update(dto)
     
     def detete(self, route_id: HashId):
         self._delete_service.delete(route_id)
---
+++ src/infrastructure/handlers/update_route.py
@@ -0,0 +1,18 @@
+import typing
+from fastapi import APIRouter
+from src.domain.value_objects import HashId
+from src.application.dto import UpdateRoutesDTO
+
+class EditRoutersService(typing.Protocol):
+    def update(self, dto: UpdateRoutesDTO): ...
+
+
+class UpdateRouteHandler:
+    def __init__(self, update_service: EditRoutersService):
+        self._update_service = update_service
+        self.router = APIRouter()
+
+        self.router.add_api_route("/route", self.update, methods=["UPDATE"])
+
+    def update(self, dto: UpdateRoutesDTO):
+        self._update_service.update(dto)
