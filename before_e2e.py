8cd5a9803fb6fff2e6f230f3bc52d954a51e3900
gaponukz <gaponukz54@gmail.com>
2023-08-29 14:43:06 +0300

    Fix CI

---
+++ before_e2e.py
@@ -0,0 +1,9 @@
+from src.infrastructure.settings import settings
+from src.infrastructure.db.mongodb import RouteRepository
+from src.infrastructure.db.json_db import RouteRepository as JsonRouteRepository
+
+config = settings.EnvSettingsExporter().load()
+db = RouteRepository(config.mongodb_url, "Cluster0")
+
+db.clear()
+db.create(JsonRouteRepository("routes.json").read_all()[0])
